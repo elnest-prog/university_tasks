17da8fad078b2aab9028fbab4f94da435c29bc72
elnest-prog <elnestprogrammer@gmail.com>
2025-07-07 23:27:45 +0500

    report 6ЛР done

--- web_django/labs/mysite/articles/views.py
+++ web_django/labs/mysite/articles/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
 
 def article_list(request):
-    return render(request, 'articles/article_list.html')
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'articles/article_list.html', {'articles': articles})
 
 
-def article_item(request, article_id):
-    data = {"id": article_id}
-    return render(request, 'articles/article_item.html', data)
+def article_item(request, slug):
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_item.html', {'article': article})
