ac25beabaf199949bc05c9674ab7adcaae415e6e
elnest <elnestprogrammer@gmail.com>
2026-01-09 11:45:43 +0500

    added lr3_2

---
+++ func_programming/lr3_2.py
@@ -0,0 +1,13 @@
+def create_pairs(arr, index=0, result=None):
+    if result is None:
+        result = []
+    
+    if index >= len(arr):
+        return result
+    
+    result.append([arr[index], arr[index] ** 2])
+    return create_pairs(arr, index + 1, result)
+
+n = 5
+array = [1, 2, 3, 4, 5]
+print("Полученный массив: ", create_pairs(array))
